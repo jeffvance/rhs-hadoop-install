ee20f7531934c84f874c722ce86c962b9ffdba6c
childsb <bchilds@redhat.com>
2013-11-21 16:03:47 -0600

    updated a few directories, setuid bit on 2.x task controller container.

--- post_install_dirs.sh
+++ post_install_dirs.sh
@@ -1,23 +1,9 @@
-#!/bin/sh
-# local	dfs.namenode.name.dir	hdfs:hadoop	drwx______
-# local	dfs.datanode.data.dir	hdfs:hadoop	drwx______
-# local	$HADOOP_LOG_DIR	hdfs:hadoop	drwxrwxr_x
-# local	$YARN_LOG_DIR	yarn:hadoop	drwxrwxr_x
-# local	yarn.nodemanager.local_dirs	yarn:hadoop	drwxr_xr_x
-# local	yarn.nodemanager.log_dirs	yarn:hadoop	drwxr_xr_x
-# local	container_executor	root:hadoop	__Sr_s___
-# local	conf/container_executor.cfg	root:hadoop	r________
-# hdfs	/	hdfs:hadoop	drwxr_xr_x
-# hdfs	/tmp	hdfs:hadoop	drwxrwxrwxt
-# hdfs	/user	hdfs:hadoop	drwxr_xr_x
-# hdfs	yarn.nodemanager.remote_app_log_dir	yarn:hadoop	drwxrwxrwxt
-# hdfs	mapreduce.jobhistory.intermediate_done_dir	mapred:hadoop	drwxrwxrwxt
-# hdfs	mapreduce.jobhistory.done_dir	mapred:hadoop	drwxr_x___
-
 yarn_nodemanager_remote_app_log_dir="/tmp/logs"
 mapreduce_jobhistory_intermediate_done_dir="/mr_history/tmp"
 mapreduce_jobhistory_done_dir="/mr_history/done"
 HADOOP_LOG_DIR=${HADOOP_LOG_DIR:-${2}/logs}
+yarn_staging_dir=/job-staging-yarn/
+task_controler=${2}/bin/container-executor
 
 #
 # the rest of these seem to be outdated.
@@ -28,12 +14,12 @@ HADOOP_LOG_DIR=${HADOOP_LOG_DIR:-${2}/logs}
 
 
 
-if [[ $# -lt 2 ]]
+if [[ $# -lt 4 ]]
 then
   echo
-  echo "Usage: $0 glustermount pathToHadoopInstall"
+  echo "Usage: $0 glustermount pathToHadoopInstall superuser group"
   echo 
-  echo "Example: $0 /mnt/gluster /opt/hadoop-2.0.5"
+  echo "Example: $0 /mnt/gluster /opt/hadoop-2.0.5 yarn hadoop"
   echo
   exit
 fi
@@ -42,9 +28,13 @@ setPerms(){
   Paths=("${!1}")
   Perms=("${!2}")
   Root_Path=$3
+  User=$4
+  Group=$5
+
   for (( i=0 ; i<${#Paths[@]} ; i++ ))
    do
     mkdir -p ${Root_Path}/${Paths[$i]}
+    chown ${User}:${Group}  ${Root_Path}/${Paths[$i]} 
     chmod ${Perms[$i]} ${Root_Path}/${Paths[$i]} 
     echo ${Paths[$i]} ${Perms[$i]}
     done 
@@ -52,11 +42,15 @@ setPerms(){
 
 echo "Setting permissions on Gluster Volume located at ${1}"
 
-paths=("/" "/tmp" "/user" "${yarn_nodemanager_remote_app_log_dir}" "${mapreduce_jobhistory_intermediate_done_dir}" "${mapreduce_jobhistory_done_dir}");
-perms=(0755 1777 0755 1777 1777 0750 );
-setPerms paths[@] perms[@] ${1}
+paths=("/" "/tmp" "/user" "/mr_history" "${yarn_nodemanager_remote_app_log_dir}" "${mapreduce_jobhistory_intermediate_done_dir}" "${mapreduce_jobhistory_done_dir}" "/mapred" "${yarn_staging_dir}");
+perms=(0755 1777 0770 0755 1777 1777 0750 0770 0770);
+setPerms paths[@] perms[@] ${1} ${3} ${4}
 
 echo "Setting local permissions, using hadoop install ${2}"
 paths=(${HADOOP_LOG_DIR});
 perms=(1777);
-setPerms paths[@] perms[@] ${2}
+setPerms paths[@] perms[@] ${2} ${3} ${4}
+
+echo "Setuid bit on task controller"
+chown root:${4} ${task_controler} ; chmod 6050 ${task_controler}
+
