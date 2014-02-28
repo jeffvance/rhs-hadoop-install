c7166922b6d3df2c60e7e475ec995cb5f17c996a
jvance <jvance@redhat.com>
2014-02-28 09:40:47 -0800

    deleted tuned and plugin code no longer used, but saved these functions in the new saved_code/ dir

--- rhs/pre_install.sh
+++ rhs/pre_install.sh
@@ -10,10 +10,9 @@
 # as an initial step by ../prep_node.sh.
 #
 # This script does the following on the host (this) node:
+#  - sets selinux to permissive mode, if enabled
 #  - tests if the fuse patch is installed in the running kernel, and if not
 #    does a yum update kernel and marks the node for a reboot,
-#  - installs the ktune.sh performance script if it exists in rhs/ or in any of
-#    its sub-dirs.
 #
 # Arguments (all positional):
 #   $1=associative array, passed by *declaration*, containing many individual
@@ -30,7 +29,7 @@
 
 #
 # constants and args
-VERSION='1.03'
+VERSION='1.04'
 eval 'declare -A _ARGS='${1#*=} # delete the "declare -A name=" portion of arg
 NODE="${_ARGS[NODE]}"
 STORAGE_INSTALL="${_ARGS[INST_STORAGE]}" # true or false
@@ -46,111 +45,6 @@ HOST_IPS=($3)
 # source common constants and functions
 source ${DEPLOY_DIR}functions
 
-
-# install_plugin: if a plugin jar file is provided in any of the included sub-
-# directories then copy it to the correct location. Otherwise, yum install the
-# rhs-hadoop plugin package, which also copies the jar file to the correct
-# directory.
-# NOTE: not currently invoked since the rhs-hadoop plugin package is now part
-#   of the RHS 2.1.1+ ISO.
-#
-function install_plugin(){
-
-  local PLUGIN_JAR='glusterfs-hadoop-.*.jar' # note: regexp not glob
-  local PLUGIN_PKG='rhs-hadoop'
-  local USR_JAVA_DIR='/usr/share/java'
-  local HADOOP_JAVA_DIR='/usr/lib/hadoop/lib/'
-  local jar=''; local out; local err
-
-  # set MATCH_DIR and MATCH_FILE vars if match
-  match_dir "$PLUGIN_JAR" "$SUBDIR_FILES"
-  if [[ -n "$MATCH_DIR" ]]; then  # found packaged plugin jar file
-    cd $MATCH_DIR
-    jar="$MATCH_FILE"
-    display "-- Installing Gluster-Hadoop plug-in ($jar)..." $LOG_INFO
-
-    # create target dirs if they does not exist
-    [[ -d $USR_JAVA_DIR ]]    || mkdir -p $USR_JAVA_DIR
-    [[ -d $HADOOP_JAVA_DIR ]] || mkdir -p $HADOOP_JAVA_DIR
-
-    # copy jar and create symlink
-    out="$(cp -uf $jar $USR_JAVA_DIR 2>&1)"
-    err=$?
-    display "plugin cp: $out" $LOG_DEBUG
-    if (( err != 0 )) ; then
-      display "ERROR: plugin copy error $err" $LOG_FORCE
-      exit 5
-    fi
-
-    rm -f $HADOOP_JAVA_DIR/$jar
-    out="$(ln -s $USR_JAVA_DIR/$jar $HADOOP_JAVA_DIR/$jar 2>&1)"
-    err=$?
-    display "plugin symlink: $out" $LOG_DEBUG
-    if (( err != 0 )) ; then
-      display "ERROR: plugin symlink error $err" $LOG_FORCE
-      exit 7
-    fi
-    cd -
-
-  elif yum -q $PLUGIN_PKG >&/dev/null ; then # pgk available
-    # yum install rhs-hadoop pkg
-    out="$(yum -y install $PLUGIN_PKG)"
-    err=$?
-    display "yum install $PLUGIN_PKG plugin: $out" $LOG_INFO
-    if (( err != 0 )) ; then
-      display "ERROR: yum install error $err" $LOG_FORCE
-      exit 8
-    fi
-
-  else
-    display "ERROR: \"$PLUGIN_PKG\" package is not available" $LOG_FORCE
-    display "       This package contains the hadoop plugin" $LOG_FORCE
-    exit 9
-  fi
-  display "   ... $PLUGIN_PKG plugin install successful" $LOG_SUMMARY
-}
-
-# apply_tuned: apply the tuned-adm peformance tuning for RHS.
-#
-function apply_tuned(){
-
-  local out; local err
-  local TUNE_PROFILE='rhs-high-throughput'
-  local TUNE_DIR="/etc/tune-profiles/$TUNE_PROFILE"
-  local TUNE_FILE='ktune.sh'
-  local TUNE_PATH="$TUNE_DIR/$TUNE_FILE"
-  local TUNE_PATH_BAK="$TUNE_PATH.orig"
-  local TUNE_PERMS=755 # rwxr-xr-x
-
-  # set MATCH_DIR and MATCH_FILE vars if match
-  match_dir "$TUNE_FILE" "$SUBDIR_FILES"
-  [[ -z "$MATCH_DIR" ]] && {
-	display "INFO: $TUNE_FILE file not supplied" $LOG_INFO
-	return; }
-
-  cd $MATCH_DIR
-
-  # replace ktune.sh
-  [[ -f $TUNE_PATH_BAK ]] || mv $TUNE_PATH $TUNE_PATH_BAK
-  out="$(cp -f $TUNE_FILE $TUNE_PATH)"
-  err=$?
-  display "$TUNE_FILE cp: $out" $LOG_DEBUG
-  if [[ ! -f $TUNE_PATH ]] ; then
-    display "ERROR: cp of $TUNE_FILE to $TUNE_DIR error $err" $LOG_FORCE
-    exit 13 
-  fi
-  chmod $TUNE_PERMS $TUNE_PATH
-
-  # run profile
-  out="$(tuned-adm profile $TUNE_PROFILE 2>&1)"
-  err=$?
-  display "tuned-adm: $out" $LOG_DEBUG
-  if (( err != 0 )) ; then
-    display "ERROR: tuned-adm error $err" $LOG_FORCE
-    exit 15
-  fi
-  cd -
-}
 
 # check_selinux: if selinux is enabled then set it to permissive. This seems
 # to be a requirement for HDP.
@@ -235,17 +129,6 @@ function install_storage(){
   echo
   display "-- Verifying FUSE patch installation:" $LOG_SUMMARY
   verify_fuse
-
-  # apply the tuned-admin rhs-high-throughput profile
-  echo
-  display "-- Applying the rhs-high-throughput profile using tuned-adm" \
-	$LOG_SUMMARY
-
-  # install the rhs-hadoop plugin
-  # NOTE: no longer needed since the RHS ISO contains the plugin
-  #echo
-  #display "-- Installing the rhs-hadoop plugin (jar file)" $LOG_SUMMARY
-  #install_plugin
 }
 
 # install_mgmt: perform the installation steps needed when the node is the
