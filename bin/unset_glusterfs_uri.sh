0222afb19042d03a1955cd52121202a088781132
sub-mod <subin@apache.org>
2014-05-13 21:59:50 +0530

    refactor

--- bin/unset_glusterfs_uri.sh
+++ bin/unset_glusterfs_uri.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-
-errcnt=0;
+#
+# set_glusterfs_uri.sh updates the hadoop core-site.xml file for the ...
+#
+# Syntax: see usage() function.
 
 _DEBUG="off"
 USERID="admin"
@@ -9,52 +10,60 @@ PASSWD="admin"
 PORT=":8080"
 PARAMS=''
 AMBARI_HOST='localhost'
-VOLUME_ID=''
+VOLNAME=''
 CLUSTER_NAME=""
 
+# debug: execute cmd in $1 if _DEBUG is set to 'on'.
+# Uses globals:
+#   _DEBUG
 function debug()
 {
  [ "$_DEBUG" == "on" ] &&  $@
 }
 
-usage () {
-  echo "Usage: unset_glusterfs_uri.sh [-u userId] [-p password] [--port port] [-h ambari_host] <VOLUME_ID>";
-  echo "";
-  echo "       [-u userId]: Optional user ID to use for authentication. Default is 'admin'.";
-  echo "       [-p password]: Optional password to use for authentication. Default is 'admin'.";
-  echo "       [--port port]: Optional port number for Ambari server. Default is '8080'. Provide empty string to not use port.";
-  echo "       [-h ambari_host]: Optional external host name for Ambari server. Default is 'localhost'.";
-  echo "       [VOLUME_ID]: Gluster Volume ID.";
-  exit 1;
+# usage: echos general usage paragraph.
+function usage () {
+  echo "Usage: unset_glusterfs_uri.sh [-u userId] [-p password] [--port port] [-h ambari_host] <VOLNAME>"
+  echo ""
+  echo "       [-u userId]: Optional user ID to use for authentication. Default is 'admin'."
+  echo "       [-p password]: Optional password to use for authentication. Default is 'admin'."
+  echo "       [--port port]: Optional port number for Ambari server. Default is '8080'. Provide empty string to not use port."
+  echo "       [-h ambari_host]: Optional external host name for Ambari server. Default is 'localhost'."
+  echo "       [VOLNAME]: Gluster Volume ID."
+  exit 1
 }
 
+# parse_cmd: parses the command line via getopt. Returns 1 on errors. Sets the
+# following globals:
+#   AMBARI_HOST
+#   _DEBUG
+#   DEBUG
+#   PASSWD
+#   PORT
+#   USERID
+#   VOLNAME
 function parse_cmd(){
 
   local OPTIONS='u:p:h:'
   local LONG_OPTS='port:,help,debug'
 
-  # defaults (global variables)
-  DEBUG=false
-  SCRIPT=$0
-
-
   local args=$(getopt -n "$SCRIPT" -o $OPTIONS --long $LONG_OPTS -- $@)
   (( $? == 0 )) || { echo "$SCRIPT syntax error"; exit -1; }
 
   eval set -- "$args" # set up $1... positional args
+
   while true ; do
-        #echo $1
 	case "$1" in
 	--help)
 		usage; exit 0
 	;;
 	--port)
-		if [ -z $2 ]; then
-		  PORT="";
+		if [[ -z "$2" ]]; then
+		  PORT=""
 		else
-		  PORT=":$2";
+		  PORT=":$2"
 		fi
-		debug echo "PORT=$2";
+		debug echo "PORT=$2"
 		PARAMS=$PARAMS" -port $2 "
 		shift 2; continue
 	;;
@@ -62,105 +71,106 @@ function parse_cmd(){
 		DEBUG=true;_DEBUG="on"; shift; continue
 	;;
 	-u)
-		USERID=$2;
-                debug echo "USERID=$USERID";
+		USERID="$2"
+                debug echo "USERID=$USERID"
 		PARAMS="-u $USERID "
 		shift 2; continue
 	;;
 	-p)
-		PASSWD=$2;
-		debug echo "PASSWORD=$PASSWD";
+		PASSWD="$2"
+		debug echo "PASSWORD=$PASSWD"
 		PARAMS=$PARAMS" -p $PASSWD "
 		shift 2; continue
 	;;
 	-h)
-		if [ -z $2 ]; then
-		  AMBARI_HOST=$AMBARI_HOST;
-		else
-		  AMBARI_HOST="$2";
-		fi
-		debug echo "AMBARI_HOST=$2";
+		[[ -n "$2" ]] && AMBARI_HOST="$2"
+		debug echo "AMBARI_HOST=$2"
 		shift 2; continue
 	;;
 	--) # no more args to parse
 		shift; break
 	;;
-	*) echo "Error: Unknown option: \"$1\""; exit -1
+	*) echo "Error: Unknown option: \"$1\""; return 1
 	;;
       esac
   done
   
-  #take care off all other arguments
-  if [[ $# -gt 1 ]]; then
-    echo "Error: Unknown values: \"$@\""; exit -1
+  #take care of all other arguments
+  if (( $# > 1 )); then
+    echo "Error: Unknown values: \"$@\""; return 1
   fi
-  if [ -z $1 ]; then
-    echo "Syntax error: VOLUME_ID is missing: \"$@\"";usage; exit -1
+  if [[ -z "$1" ]]; then
+    echo "Syntax error: VOLNAME is missing: \"$@\""; usage; return 1
   else
-    VOLUME_ID="$1";
+    VOLNAME="$1"
   fi
 
   eval set -- "$@" # move arg pointer so $1 points to next arg past last opt
 
-  if [[ $DEBUG == true ]] ; then
-	debug echo "DEBUGGING ON"
-  fi
+  [[ $DEBUG == true ]] && debug echo "DEBUGGING ON"
+
+  return 0
 }
 
-########################
-## currentClusterName()
-########################
-currentClusterName () {
+# currentClusterName: sets the CLUSTER_NAME based on the value in the ambari
+# config file. Returns 1 on errors.
+# Set globals:
+#   CLUSTER_NAME
+function currentClusterName () {
 
-  line=`curl -s -u $USERID:$PASSWD "$AMBARIURL/api/v1/clusters/" | grep -E "cluster_name" | sed "s/\"//g"`;
-  if [ -z "$line" ]; then
-    echo "[ERROR] Cluster was not found in server response.";
-    exit 1;
+  local line=`curl -s -u $USERID:$PASSWD "$AMBARIURL/api/v1/clusters/" | grep -E "cluster_name" | sed "s/\"//g"`
+  local line1; local propLen; local lastChar
+  local key; local value; local keyvalue=()
+
+  if [[ -z "$line" ]]; then
+    echo "ERROR: Cluster was not found in server response."
+    return 1
   fi
 
   debug echo "########## LINE = "$line
 
-  line1=$line
+  line1="$line"
   propLen=${#line1}
   lastChar=${line1:$propLen-1:1}
-  if [ "$lastChar" == "," ]; then
-    line1=${line1:0:$propLen-1}
-  fi
+  [[ "$lastChar" == "," ]] && line1=${line1:0:$propLen-1}
 
   OIFS="$IFS"
   IFS=':'
-  read -a keyvalue <<< "${line1}"
+  read -a keyvalue <<< "$line1"
   IFS="$OIFS"
   key=${keyvalue[0]}
   value="${keyvalue[1]}"
 
-  value=`echo $value | sed "s/[\"\,\ ]//g"`
+  value=$(echo "$value" | sed "s/[\"\,\ ]//g")
   debug echo "########## VALUE = "$value
-  if [[ ! -z $value ]]; then   
-    CLUSTER_NAME="$value" ; 
+  if [[ ! -z "$value" ]]; then   
+    CLUSTER_NAME="$value"
   else
-    echo "Error: Cluster not found"
-    exit 1
-  fi
+    echo "ERROR: Cluster not found"
+    return 1
+  fi 
 }
-
 
 
 ## ** main ** ##
 
-parse_cmd $@
+# defaults (global variables)
+DEBUG=false
+SCRIPT=$0
+
+parse_cmd $@ || exit -1
 
 AMBARIURL="http://$AMBARI_HOST$PORT"
 debug echo "########## AMBARIURL = "$AMBARIURL
 
-currentClusterName
+currentClusterName || exit 1
 debug echo "########## CLUSTER_NAME = "$CLUSTER_NAME
-PARAMS=$PARAMS" remove_volume $AMBARI_HOST $CLUSTER_NAME core-site "$VOLUME_ID
+
+PARAMS=$PARAMS" remove_volume $AMBARI_HOST $CLUSTER_NAME core-site "$VOLNAME
 PARAMS=`echo $PARAMS | sed "s/\"//g"`
 debug echo "########## PARAMS = "$PARAMS
 	
 debug echo "sh ./ambari_config.sh $PARAMS"
-sh ./ambari_config.sh $PARAMS	|| ((errcnt++))
+sh ./ambari_config.sh $PARAMS || exit 
 
-(( errcnt > 0 )) && exit 1
 exit 0
