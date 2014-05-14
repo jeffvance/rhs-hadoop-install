0d8fc8f35b22190673b953b93f664235ef6ee7bf
Scott McClellan <scott.j.mcclellan@gmail.com>
2014-05-14 15:54:58 -0700

    Update gen_ports.sh
    Fixed mismatching quotes...

--- bin/gen_ports.sh
+++ bin/gen_ports.sh
@@ -10,6 +10,6 @@ AMBARI_PORTS='8080:tcp 8440-8441:tcp'
 #  Default Hadoop Ports used by Hortonworks Hadoop (HDP) are documented here:
 #  http://docs.hortonworks.com/HDPDocuments/HDP1/HDP-1.2.1/bk_reference/content/reference_chap2_1.html
 #
-HADOOP_PORTS='50070:tcp 50470:tcp 8020-9000:tcp 50075:http 50475:https 50010:tcp 50020:tcp 50090:http"
+HADOOP_PORTS='50070:tcp 50470:tcp 8020-9000:tcp 50075:http 50475:https 50010:tcp 50020:tcp 50090:http'
 
 echo "$GLUSTER_PORTS $AMBARI_PORTS $HADOOP_PORTS"
