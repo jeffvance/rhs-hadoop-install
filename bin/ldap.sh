7e5293c1fb628d95f5989cf144b1a1be15d9a134
jvance <jvance@redhat.com>
2014-06-09 23:28:24 -0700

    more fixes for ldap.sh

--- bin/ldap.sh
+++ bin/ldap.sh
@@ -8,12 +8,14 @@
 #   2+=list of additional users to add, eg. "tom sally ed", (optional)
 
 PREFIX="$(dirname $(readlink -f $0))"
+ssh=''; ssh_close=''
 YARN_NODE="$1"; shift
 [[ -z "$YARN_NODE" ]] && {
   echo "Syntax error: the yarn-master node is the first arg and is required";
   exit -1; }
 
-[[ "$HOSTNAME" == "$YARN_NODE" ]] && ssh='' || ssh="ssh $YARN_NODE"
+[[ "$HOSTNAME" != "$YARN_NODE" ]] && {
+  ssh="ssh $YARN_NODE '"; ssh_close="'"; }
 YARN_DOMAIN="$(eval "$ssh hostname -d")"
 [[ -z "$YARN_DOMAIN" ]] && YARN_DOMAIN="$YARN_NODE"
 
@@ -23,29 +25,25 @@ ADMIN='admin'
 PASSWD='admin123' # min of 8 chars
 
 # on the server:
-eval "$ssh
-	yum -y install ipa-server		  && \
+eval "$ssh 
+	yum -y install ipa-server		 && \
 	ipa-server-install -U --hostname=$YARN_NODE --realm=HADOOP \
 		--domain=$YARN_DOMAIN --ds-password=$PASSWD \
-		--admin-password=$PASSWD && \
+		--admin-password=$PASSWD	 && \
 	echo $PASSWD | kinit $ADMIN && \
-        echo \"hadoop group description\" | ipa group-add hadoop && \
+        ipa group-add hadoop --desc hadoop-group && \
 	for user in $USERS; do
-            IN=\"$user\"
-            set -- \"$IN\"
-            IFS=\",\"; declare -a Array=($*)
-            u=\"${Array[0]}\"
-            f=\"${Array[1]}\"
-            l=\"${Array[2]}\"
-	    ipa user-add $u --first $f --last $l 
-            ipa group-add-member hadoop --users=$u
-        done		&& \ "
+	    ipa user-add $user --first $user --last $user 
+        done					 && \
+        ipa group-add-member hadoop --users=${USERS// /,}
+      $ssh_close"
 err=$?
 if (( err != 0 )) ; then
-  echo "ERROR $err: ipa-server: adding group-users"
+  echo "ERROR $err: ipa-server: adding users: $USERS"
   exit 1
 fi
 
 # on the clients:
 ssh mrg42 "yum -y install ipa-client"
-ssh mrg42 "ipa-client-install --enable-dns-updates --domain $YARN_DOMAIN	--server $YARN_NODE --realm HADOOP"
+ssh mrg42 "ipa-client-install --enable-dns-updates --domain $YARN_DOMAIN \
+	--server $YARN_NODE --realm HADOOP"
