fbb7947c2a8a1621418fec19c2e0b53438c19c7b
root <root@mrg41.lab.bos.redhat.com>
2014-06-09 23:01:59 -0400

    update of script : basically works with exception of the USERS variable... which i didnt test yet... seemed something was funny with it ?

--- bin/ldap.sh
+++ bin/ldap.sh
@@ -8,23 +8,28 @@
 #   2+=list of additional users to add, eg. "tom sally ed", (optional)
 
 PREFIX="$(dirname $(readlink -f $0))"
-YARN_NODE="$1"; shift
+YARN_NODE="$1"; shift 
 [[ "$HOSTNAME" == "$YARN_NODE" ]] && ssh='' || ssh="ssh $YARN_NODE"
-USERS="$($PREFIX/gen_users.sh)" # required hadoop users
-USERS+=" $@" # add any additional passed-in users
+USERS="sally" # required hadoop users
+### USERS+=" $@" # add any additional passed-in users
+
 YARN_DOMAIN="$(eval "$ssh hostname -d")"
+
 
 ADMIN='admin'
 PASSWD='admin123' # min of 8 chars
 
+
+echo "YARN server = $YARN_DOMAIN "
+
 # on the server:
 eval "$ssh
 	yum -y install ipa-server		&& \
-	echo $PASSWD | kinit $ADMIN		&& \
 	ipa-server-install -U --hostname=$YARN_NODE --realm=HADOOP \
 		--domain=$YARN_DOMAIN --ds-password=$PASSWD \
-		--admin-password=&& $PASSWD \
-	ipa group-add hadoop			&& \
+		--admin-password=$PASSWD && \
+	echo $PASSWD | kinit $ADMIN && \
+        ipa group-add hadoop  --desc="hadoop user"  && \
 	for user in $USERS; do
     	    ipa user-add $user
 	done					&& \
@@ -37,6 +42,6 @@ if (( err != 0 )) ; then
 fi
 
 # on the clients:
-yum -y install ipa-client
-ipa-client-install --enable-dns-updates --domain $YARN_DOMAIN \
-	--server $YARN_NODE --realm HADOOP
+ssh mrg42 "yum -y install ipa-client"
+ssh mrg42 "ipa-client-install --enable-dns-updates --domain $YARN_DOMAIN \
+	--server $YARN_NODE --realm HADOOP"
