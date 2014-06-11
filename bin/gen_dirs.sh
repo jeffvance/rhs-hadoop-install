6529bad314f738dfe9b815baba888f3da7a72b41
jvance <jvance@redhat.com>
2014-06-11 07:46:01 -0700

    remove hbase dir and user since out of scope for Denali

--- bin/gen_dirs.sh
+++ bin/gen_dirs.sh
@@ -33,7 +33,7 @@ shift $((OPTIND-1))
   echo -n "mapred:0770:mapred mapred/system:0755:mapred tmp:1777:yarn \
 user:0775:yarn mr-history:0755:yarn tmp/logs:1777:yarn \
 mr-history/tmp:1777:yarn mr-history/done:0770:yarn job-staging-yarn:0770:yarn \
-app-logs:1777:yarn hbase:0770:hbase apps:0775:hive apps/webhcat:0775:hcat "
+app-logs:1777:yarn apps:0775:hive apps/webhcat:0775:hcat "
 
 [[ -n "$LOCAL" ]] && \
   echo -n "mapredlocal:0755:root "
--- bin/gen_users.sh
+++ bin/gen_users.sh
@@ -3,10 +3,4 @@
 # gen_users.sh outputs the required hadoop users. This is the only source for
 # these users. No attempt is made to create nor validate these users.
 
-HBASE_U='hbase'
-HCAT_U='hcat'
-HIVE_U='hive'
-MAPRED_U='mapred'
-YARN_U='yarn'
-
-echo "$MAPRED_U $YARN_U $HBASE_U $HCAT_U $HIVE_U"
+echo "mapred yarn hcat hive"
