902c0e7359ed1f5d77b8ef879520c2fb2e1859d2
root <root@mrg41.lab.bos.redhat.com>
2014-06-10 22:01:54 -0400

    Added password and -U option to ipa client install, also a comment about removing old certs

--- bin/ldap_clients.sh
+++ bin/ldap_clients.sh
@@ -11,6 +11,11 @@ IPA_DOMAIN="$1"; shift
 IPA_REALM="$1";  shift
 CLIENT_NODES="$@"
 
+### JEFF : Decide how to parameterize these.
+### For now, I think its okay to hardcode the values.
+ADMIN="admin"
+PASSWD="admin123"
+
 # Remaining args: IPA Clients 
 
 echo "***************************************"
@@ -25,8 +30,9 @@ for node in $CLIENT_NODES; do
 	yum -y install ipa-client
         # uninstall ipa-client-install for idempotency
         ipa-client-install --uninstall -U
+        echo "Uninstalled ipa : If there are old cert errors, also run rm -rf /etc/ipa/ca.crt"
         ipa-client-install --enable-dns-updates --domain $IPA_DOMAIN \
-		--server $IPA_SERVER --realm $IPA_REALM
+		--server $IPA_SERVER --realm $IPA_REALM -p $ADMIN -w $PASSWD -U
         err=\$?
         (( err != 0 )) && {
 	  echo "ERROR \$err: ipa-client-install on \$node"; exit 1; }
