e82f9964c220c797a7476aa583480ffb88876af6
childsb <bchilds@redhat.com>
2014-07-16 14:38:55 -0400

    Auto-create the container-executer.cfg contents.

--- setup_container_executor.sh
+++ setup_container_executor.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
+create_conttainer_executor() {
+  rm ${task_cfg}
+  touch ${task_cfg} 
+  cat <<EOF > ${task_cfg} 
+yarn.nodemanager.linux-container-executor.group=hadoop
+banned.users=yarn
+min.user.id=1000
+allowed.system.users=mapred
+EOF
+
+}
+
+
 process_user=yarn
 process_group=hadoop
 task_controller=/usr/lib/hadoop-yarn/bin/container-executor
 task_cfg=/etc/hadoop/conf/container-executor.cfg
-
+create_conttainer_executor
 echo "Configuring the Linux Container Executor for Hadoop"
 chown root:${process_group} ${task_controller} ; chmod 6050 ${task_controller}
 chown root:${process_group} ${task_cfg}
