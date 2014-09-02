12a90ae19b461f65fc5e58175c747f5ee875d27f
jvance <jvance@redhat.com>
2014-09-02 16:28:10 -0700

    more progress msgs to setup_ambari-server

--- bin/setup_ambari_server.sh
+++ bin/setup_ambari_server.sh
@@ -23,6 +23,7 @@ source $PREFIX/functions
 
 # stop ambari-server if running
 if (( SERVER_ALREADY_INSTALLED )) ; then
+  echo "...stopping ambari-server..."
   ambari-server stop 2>&1
   err=$?
   (( err != 0 )) && {
@@ -30,14 +31,14 @@ if (( SERVER_ALREADY_INSTALLED )) ; then
     ((warncnt++)); }
 fi
 
-# wget the ambari repo
+echo "...wget-ing ambari-server repo..."
 get_ambari_repo
 err=$?
 (( err != 0 )) && {
   echo "ERROR $err: can't wget ambari repo";
   exit 1; }
 
-# install the ambari server
+echo "...yum installing ambari-server..."
 yum -y install ambari-server 2>&1
 err=$?
 (( err != 0 )) && {
@@ -48,6 +49,7 @@ err=$?
 # note: must be done after the yum install and before the setup step
 for dir in $RM_SERVICE_DIRS; do
     if [[ -f $SERVICE_PATH/$dir ]] ; then
+      echo "...deleting un-needed service directory $dir..."
       rm -rf $SERVICE_PATH/$dir
       err=$?
       (( err != 0 )) && {
@@ -56,8 +58,8 @@ for dir in $RM_SERVICE_DIRS; do
     fi
 done
 
-# setup the ambari-server
 # note: -s accepts all defaults with no prompting
+echo "...setup ambari-server..."
 ambari-server setup -s 2>&1
 err=$?
 (( err != 0 )) && {
@@ -66,30 +68,31 @@ err=$?
 
 # set the active=true flag in the meta info xml file so that ambari is
 # aware of alternative HCFS file systems, like RHS/glusterfs.
+echo "...set \"active\" config flag..."
 sed -i -e "s/$ACTIVE_FALSE/$ACTIVE_TRUE/" $METAINFO_PATH
 
-# start the ambari server
+echo "...starting ambari-server..."
 ambari-server start 2>&1
 err=$?
 (( err != 0 )) && {
   echo "ERROR $err: ambari-server start";
   exit 1; }
 
-# ensure restart of the server after a reboot
+echo "...persisting ambari-server..."
 chkconfig ambari-server on 2>&1
 err=$?
 (( err != 0 )) && {
   echo "WARN $err: chkconfig ambari-server on";
   ((warncnt++)); }
 
-# set selinux to permissive mode
+echo "...selinux permissive mode..."
 setup_selinux
 err=$?
 (( err != 0 )) && {
   echo "ERROR $err: setting up selinux";
   exit 1; }
 
-# turn off iptables
+echo "...disabling iptables..."
 setup_firewall
 err=$?
 (( err != 0 )) && {
