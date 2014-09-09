45a5c75e7d207559cc76f63d4a8849cdf5cc322c
jvance <jvance@redhat.com>
2014-09-09 11:54:41 -0700

    added hdp 2.1 users to gen_users script

--- bin/gen_users.sh
+++ bin/gen_users.sh
@@ -3,4 +3,4 @@
 # gen_users.sh outputs the required hadoop users. This is the only source for
 # these users. No attempt is made to create nor validate these users.
 
-echo "mapred yarn hcat hive"
+echo "mapred yarn hcat hive ambari-qa hbase tez zookeeper oozie falcon"
