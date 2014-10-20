51d57c4b36e93f82cc79dd4a7ee56c2a1ad3c86f
jvance <jvance@redhat.com>
2014-10-20 10:06:04 -0700

    late add of gen_ambari_repo_url

---
+++ bin/gen_ambari_repo_url.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# gen_ambari_repo_url.sh outputs the ambari repo file url for ambari 1.6,
+# hdp 2.1
+
+echo "http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.6.1/ambari.repo"
