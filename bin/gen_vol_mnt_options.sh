f0b33d8c008ee6acc4356936f2599e999567a11d
jvance <jvance@redhat.com>
2015-04-10 11:49:27 -0700

    use-readdirp=no is now a warning, no longer required

--- bin/gen_vol_mnt_options.sh
+++ bin/gen_vol_mnt_options.sh
@@ -8,17 +8,17 @@
 
 # NOTE: we flip-flop on returning entry-timeout=0 and attribute-timeout=0 due 
 #   kernel/fuse bugs/issues. We need these values set to 1(default) to get
-#   acceptable performance, but due kernel/fuse/ESTALE issues, we have to 
+#   acceptable performance, but due to kernel/fuse/ESTALE issues, we have to 
 #   temporarily set them to 0, meaning no fuse caching. As of Apr 2015 we are
 #   returning "" for required fuse mount options (meaning there are no required
-#   volume mount options), and "*-timeout=0" for warnings.
+#   volume mount options), and "*-timeout=0 and "use-readdirp=no" for warnings.
 # Args:
 #   -l : return output consistent with what is found in the /var/run/gluster
-#        "state" file. Otherwise, return output consistent with the contents of
+#        "state" file. Otherwise, return format consistent with the contents of
 #	 /etc/fstab.
 #   -w : return mnt options we want to warn about. The output is formated based
 #	 on the whether or not -l was supplied. Default is to return the
-#        required mnt options.
+#        required mnt format.
 
 LIVE=0 # false
 WARN=0 # false
@@ -40,10 +40,10 @@ shift $((OPTIND-1))
 
 if (( LIVE )) ; then # return output consistent with gluster state format
   (( WARN )) && \
-    echo 'entry_timeout=0.000000 attribute_timeout=0.000000' ||
-    echo 'use_readdirp=0' # required
+    echo 'entry_timeout=0.000000 attribute_timeout=0.000000 use_readdirp=0' \
+    || echo '' # no required mnt settings
 else # return output consistent with fstab format
   (( WARN )) && \
-    echo 'entry-timeout=0,attribute-timeout=0' ||
-    echo 'use-readdirp=no' # required
+    echo 'entry-timeout=0,attribute-timeout=0,use-readdirp=no' \
+    || echo '' # no required mnt settings
 fi
