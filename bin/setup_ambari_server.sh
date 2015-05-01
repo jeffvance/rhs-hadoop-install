ec2cb9fc390b25f5b1db1bd4e2d6ca53ea45fb16
jvance <jvance@redhat.com>
2015-05-01 14:51:41 -0700

    enable all hdp stacks for GlusterFS

--- bin/setup_ambari_server.sh
+++ bin/setup_ambari_server.sh
@@ -2,10 +2,10 @@
 #
 # setup_ambari_server.sh: unless the ambari-server is already running, install
 # and start the ambari-server on this node (localhost), and set the active flag
-# to true in the meta info xml file, which makes ambari aware of alternative HCFS
-# file systems, like RHS/glusterfs. SELinux is set to permissive mode. iptables
-# is turned off. Some service related directories are remove for unsupported
-# services.
+# to true in the meta info xml files for all HDP stacks. This causes ambari to
+# be aware of alternative HCFS's (hadoop-comptatible-file-systems), such as RHS.
+# SELinux is set, to permissive mode. iptables is turned off. Some service HDP
+# related directories are remove for unsupported services.
 # NOTE: if FORCE_AMBARI is set then even if the server is running it will be re-
 #   yum installed and started.
 # Syntax:
@@ -52,16 +52,19 @@ function parse_cmd() {
   return 0
 }
 
-# ambari_server: install, start, persist the ambari-server. Returns 1 on errors.
+# ambari_server: install, start, persist the ambari-server unless it is already
+# running (and not overriden by the FORCE_AMBARI flag). "enable" glusterfs by
+# setting active=true in all HDP metainfo files. Remove certain HDP services 
+# until they are supported by Red Hat.  Returns 1 on errors.
 function ambari_server() {
 
   local AMBARI_SERVER_PID='/var/run/ambari-server/ambari-server.pid'
-  local HDP_DIR='/var/lib/ambari-server/resources/stacks/HDP/2.1.GlusterFS'
-  local METAINFO_PATH="$HDP_DIR/metainfo.xml"
-  local SERVICE_PATH="$HDP_DIR/services"
+  local HDP_DIR='/var/lib/ambari-server/resources/stacks/HDP'
+  local GLUSTER_DIR='[0-9]*.GlusterFS' # all glusterfs stacks
+  local METAINFO_FILE='metainfo.xml'
   local RM_SERVICE_DIRS='FALCON STORM' # dirs to be deleted
   local ACTIVE_FALSE='<active>false<'; ACTIVE_TRUE='<active>true<'
-  local dir
+  local dir; local f; local gluster_dirs; local service_dir
 
   if [[ -f $AMBARI_SERVER_PID ]] && \
      which ambari-server >& /dev/null && \
@@ -98,20 +101,26 @@ function ambari_server() {
     echo "ERROR $err: ambari server install";
     return 1; }
 
-  # delete un-needed service related directories
+  # find all HDP GlusterFS stack dirs
+  gluster_dirs="$(ls -d $HDP_DIR/$GLUSTER_DIR)"
+  echo "...GlusterFS HDP stack directories: $gluster_dirs"
+
+  # delete un-needed service related directories in each glusterfs dir
   # note: must be done after the yum install and before the setup step
   echo "...removing certain service directories until they are supported:"
   echo "...  $RM_SERVICE_DIRS, if present..."
-  for dir in $RM_SERVICE_DIRS; do
-      dir="$SERVICE_PATH/$dir"
-      if [[ -d $dir ]] ; then
-	echo "...deleting un-needed service directory $dir..."
-	rm -rf $dir
-	err=$?
-	(( err != 0 )) && {
-	  echo "WARN $err: deleting $dir directory";
-	  ((warncnt++)); }
-      fi
+  for dir in $gluster_dirs; do
+      for service_dir in $RM_SERVICE_DIRS; do
+	 f="$dir/$service_dir" # directory
+	 if [[ -d $f ]] ; then
+	   echo "...deleting un-needed service directory $f..."
+	   rm -rf $f
+	   err=$?
+	   (( err != 0 )) && {
+	     echo "WARN $err: deleting $f directory";
+	     ((warncnt++)); }
+	 fi
+      done
   done
 
   # note: -s accepts all defaults with no prompting
@@ -122,10 +131,14 @@ function ambari_server() {
     echo "ERROR $err: ambari server setup";
     return 1; }
 
-  # set the active=true flag in the meta info xml file so that ambari is
+  # set the active=true flag in all HDP metainfo xml files so that ambari is
   # aware of alternative HCFS file systems, like RHS/glusterfs.
-  echo "...set \"active\" config flag..."
-  sed -i -e "s/$ACTIVE_FALSE/$ACTIVE_TRUE/" $METAINFO_PATH
+  echo "...setting \"active\" config flag..."
+  for dir in $gluster_dirs; do
+     f="$dir/$METAINFO_FILE"
+     echo "... in file $f..."
+     sed -i -e "s/$ACTIVE_FALSE/$ACTIVE_TRUE/" $f
+  done
 
   echo "...starting ambari-server..."
   ambari-server start 2>&1
